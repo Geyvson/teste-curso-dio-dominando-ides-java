c4de53d522a381892df7a3b844deb66c9a17d63d
GEYVSON <txemr@live.com>
2023-03-12 16:43:52 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroProgrma.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroProgrma {
+
+	public static void main(String[] args) {
+		Gato gato = new Gato("Chiquinha", "Branca", 5);
+		
+		System.out.println(gato);
+		
+	}
+
+}
